86f75194fa126e2d12ebcc7f8b4be6598ac5bfde
pedro <pptagle1@miuandes.cl>
2025-03-27 15:54:21 -0300

    ahora  funciona el mensaje

--- twitter/app/controllers/pages_controller.rb
+++ twitter/app/controllers/pages_controller.rb
@@ -3,17 +3,25 @@ class PagesController <  ApplicationController
     end
     def about
     end
+
     def contact
+      @message = Message.new
     end
+  
     def submit_contact
-        @message = Message.new(message_params)
-        if @message.save
-          flash[:notice] = "Thank you for your message, we will contact you soon."
-          redirect_to contact_path
-        else
-          flash[:alert] = "There was an error. Please try again."
-          render :contact
-        end
+      @message = Message.new(message_params)
+      if @message.save
+        flash[:notice] = "Thank you for your message, we will contact you soon."
+        redirect_to contact_path
+      else
+        flash[:alert] = "There was an error submitting your message."
+        render :contact
       end
-      
+    end
+  
+    private
+  
+    def message_params
+      params.require(:message).permit(:name, :email, :message)
+    end
 end
